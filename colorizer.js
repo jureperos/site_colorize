62c1044c3f30fd73f7dc8d1993319f00dd873cb5
jureperos <jure.peros@gmail.com>
2023-04-23 14:04:44 +0200

    added color picker, and color send message

--- colorizer.js
+++ colorizer.js
@@ -1,20 +1,22 @@
 function getTargetNode(event) {
     const node = event.target;
-    console.log(node);
+    node.style.background = 'red'
+    console.log(node.style);
 }
+
 
 
 function elementDisplayer(active) {
     if (active) {
-        document.addEventListener('click', getTargetNode)
+        document.addEventListener('click', getTargetNode);
     } else {
-        document.removeEventListener('click', getTargetNode)
+        document.removeEventListener('click', getTargetNode);
         console.log('switch off');
     }
 };
 
 
-
+// recieve and parse messages
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         console.log(request)
--- popup.js
+++ popup.js
@@ -1,17 +1,30 @@
 const onOffswitch = document.getElementById('on-off-input');
-console.log(typeof onOffswitch.value, onOffswitch.value);
+const colorPick = document.getElementById('pick-color')
 
-//this sends a message that the colorizer.js is listening to
-async function sendMessage(onOff) {
+// -------- Send on/off state ----------
+const sendOnOff = async function sendMessage(onOff) {
     const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
     const response = await chrome.tabs.sendMessage(tab.id, { isToggleOn: onOff });
     console.log(response);
 }
 
 
-//
 onOffswitch.addEventListener('click', () => {
     if (onOffswitch.checked) {
-        sendMessage(true);
-    } else sendMessage(false);
+        sendOnOff(true);
+    } else sendOnOff(false);
 })
+
+// ------- Send picked color ---------
+const sendColor = async function sendMessage(color) {
+    const [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
+    const response = await chrome.tabs.sendMessage(tab.id, { pickedColor: color });
+    console.log(response);
+}
+
+function getColor(event) {
+    const pickedColor = event.target.value
+    sendColor(pickedColor)
+}
+
+colorPick.addEventListener('input', getColor)
